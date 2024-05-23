fc664e9643151578cf6ce8362a38fa6b55ce7183
amestre <agustinamestre@macro.com.ar>
2024-05-23 20:16:42 -0300

    Exercise03 Read Input Challenge

---
+++ src/main/java/com/HackerRank/exercises/exercise03/Exercise03.java
@@ -0,0 +1,16 @@
+package com.HackerRank.exercises.exercise03;
+
+import java.util.Scanner;
+
+public class Exercise03 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int c = Integer.parseInt(scan.nextLine());
+        double b = Double.parseDouble(scan.nextLine());
+        String a = scan.nextLine();
+
+        System.out.println("String: " + a);
+        System.out.println("Double: " + b);
+        System.out.println("Int: " + c);
+    }
+}
